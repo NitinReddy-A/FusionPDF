fcc334c0c8c0943a0e74ec1f42b5fcdba6f8d853
NitinReddy-A <114919978+NitinReddy-A@users.noreply.github.com>
2024-06-18 23:07:32 +0530

    New method for img extraction

---
+++ try/tryimg.py
@@ -0,0 +1,9 @@
+from pikepdf import Pdf, Name, PdfImage
+
+old_pdf = Pdf.open("demo.pdf")
+page1 = old_pdf.pages[0]
+print(list(page1.images.keys())) #['/Im0']
+
+raw_image = page1.images['/Im0']
+pdf_img = PdfImage(raw_image)
+pdf_img.extract_to(fileprefix="test1")
