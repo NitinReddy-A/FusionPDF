21aa55d082f16fcba2066c97465879c1f4a17626
NitinReddy-A <114919978+NitinReddy-A@users.noreply.github.com>
2024-07-02 00:56:08 +0530

    New

--- try/new.py
+++ try/new.py
@@ -2,10 +2,14 @@ import fitz
 
 def remove_images(input_pdf):
     doc = fitz.open(input_pdf)
+    doc.extract_image()
     for page in doc:
-        img_list = page.get_image_info()
+        page.clean_contents()
+        img_list = page.get_images()
         for img in img_list:
             print(img)
+            xref = img[0]
+            doc.extract_image(xref=xref)
 
 
-remove_images("demo.pdf")
+remove_images("documents/demo.pdf")
