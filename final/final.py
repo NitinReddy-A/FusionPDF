22e7c0f84ff65b8eeb1abe958e061e169eefbb71
NitinReddy-A <114919978+NitinReddy-A@users.noreply.github.com>
2024-07-08 23:34:29 +0530

    New

--- final/final.py
+++ final/final.py
@@ -8,8 +8,8 @@ import convertapi
 convertapi.api_secret = '4OYbALQ2RREClhvm'
 
 # Open the PDF document
-doc = fitz.open(r"documents/demo1.pdf") #---------> Specify the input file
-new_pdf_path = r"documents/TranslatedDemo1.pdf" #-----------> Specify the output file
+doc = fitz.open(r"documents/demo3.pdf") #---------> Specify the input file
+new_pdf_path = r"documents/TranslatedDemo3.pdf" #-----------> Specify the output file
 
 #Specify the file path here
 docf = 'documents/TranslatedDemo1' #-----------------> new_pdf_path ***but without extension***
@@ -42,17 +42,20 @@ for page_index in range(doc.page_count):
 
     # Iterate through each image and save it in the images folder
     for img_index, img in enumerate(image_list):
-        if img[1] == 0:
-            print(img)
-            bbox = page.get_image_bbox(img)
-            xref = img[0] # get the XREF of the image
-            pix = pymupdf.Pixmap(doc, xref) # create a Pixmap
-            if pix.n - pix.alpha > 3: # CMYK: convert to RGB first
-                pix = pymupdf.Pixmap(pymupdf.csRGB, pix)
-            pix.save(f"images/page{page_index}-image{img_index}.png") # save the image as png
-            pix = None
-            new_page.insert_image(bbox, stream=open(f"images/page{page_index}-image{img_index}.png", "rb").read())
-            print(f"Image {img_index} on page {page_index}: {bbox}")
+        try:
+            if img[1] == 0:
+                print(img)
+                bbox = page.get_image_bbox(img)
+                xref = img[0] # get the XREF of the image
+                pix = pymupdf.Pixmap(doc, xref) # create a Pixmap
+                if pix.n - pix.alpha > 3: # CMYK: convert to RGB first
+                    pix = pymupdf.Pixmap(pymupdf.csRGB, pix)
+                pix.save(f"images/page{page_index}-image{img_index}.png") # save the image as png
+                pix = None
+                new_page.insert_image(bbox, stream=open(f"images/page{page_index}-image{img_index}.png", "rb").read())
+                print(f"Image {img_index} on page {page_index}: {bbox}")
+        except Exception as e:
+            print(f"An error occurred while extracting image: {e}")
 
 # Save the new PDF document
 new_doc.save(new_pdf_path)
