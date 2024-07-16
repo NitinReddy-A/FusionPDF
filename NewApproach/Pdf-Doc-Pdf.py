d71c5f3e994d8819c0bf3994e318195953f64dca
NitinReddy-A <114919978+NitinReddy-A@users.noreply.github.com>
2024-07-16 18:08:00 +0530

    NewApproach

---
+++ NewApproach/Pdf-Doc-Pdf.py
@@ -0,0 +1,16 @@
+# Code snippet is using the ConvertAPI Python Client: https://github.com/ConvertAPI/convertapi-python
+
+import convertapi
+
+convertapi.api_secret = '4OYbALQ2RREClhvm'
+
+#Specify the file path here
+doc = 'documents/TranslatedDemo3'
+
+convertapi.convert('docx', {
+    'File': f'{doc}.pdf' 
+}, from_format = 'pdf').save_files('documents')
+
+#convertapi.convert('pdf', {
+#    'File': f'{doc}.docx' 
+#}, from_format = 'docx').save_files('documents')
